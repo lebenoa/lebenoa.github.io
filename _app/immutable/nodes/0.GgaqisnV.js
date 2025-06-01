0ea9d9d04669f3a964a0cefb24adaf5238bf4fc1
lebenoa <lebenoa@users.noreply.github.com>
2025-06-01 16:05:08 +0000

    deploy: 8e5b0b98f91f2945f313fab36744ae51dee58a7b

---
+++ _app/immutable/nodes/0.GgaqisnV.js
@@ -0,0 +1,1 @@
+import{c as n,a}from"../chunks/ls_0JOi6.js";import{g as s}from"../chunks/tEZZukQM.js";import{s as p}from"../chunks/BUCFW1C2.js";const c=!0,i=!1,u=Object.freeze(Object.defineProperty({__proto__:null,prerender:c,ssr:i},Symbol.toStringTag,{value:"Module"}));function d(o,r){var e=n(),t=s(e);p(t,()=>r.children),a(o,e)}export{d as component,u as universal};
